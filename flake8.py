0124f439cc58e3b7df0cd501b06c582caedffd08
Dan Bate <dan.bate@gmail.com>
2019-05-22 11:15:47 +0100

    added missing file

---
+++ flake8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+from githooks import args
+
+files = [f for f in args.pre_commit().files if re.match('.*\.py$', f)]
+
+if files:
+    sys.exit(subprocess.call(['flake8'] + files))
