59da3d061ded52b39e7cf9e7d65568eec3a90dc3
Dan Bate <dan.bate@gmail.com>
2020-04-22 15:59:24 +0100

    added scripts for black, isort and mypy

---
+++ black.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import re
+
+from githooks import args
+
+files = [f for f in args.pre_commit().files if re.match('.*\.py$', f)]
+
+if files:
+    sys.exit(subprocess.call(['black', '--check'] + files))
